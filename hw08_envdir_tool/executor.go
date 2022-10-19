ce2fc319c29517efd74662e10284ad5899754d3d
gor4yma_dev <zubk-igr@rambler.ru>
2022-10-19 15:31:14 +0600

    hw08 is somplited

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
+const (
+	ExitCodeIOError         int = 17
+	ExitCodeCommandNotFound int = 111
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) < 1 {
+		return ExitCodeCommandNotFound
+	}
+	command, args := cmd[0], cmd[1:]
+	cmdResult := exec.Command(command, args...) //nolint
+	cmdResult.Env = env.Strings()
+	cmdResult.Stdin = os.Stdin
+	cmdResult.Stdout = os.Stdout
+	cmdResult.Stderr = os.Stderr
+
+	if err := cmdResult.Run(); err != nil {
+
+		return ExitCodeIOError
+	}
+	return 0
 }
